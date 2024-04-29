c3f358689a60f4f81ce9259c6b79f7142b45bcd6
Igor Malyugin <i.malyugin00@gmail.com>
2024-04-29 22:02:19 +0300

    home & footer adaptive

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,5 @@
 const swiper1 = new Swiper('.products-slider-1', {
-  loop: true,
+  loop: false,
   spaceBetween: 18,
   slidesPerView: 3,
 
@@ -8,9 +8,23 @@ const swiper1 = new Swiper('.products-slider-1', {
     prevEl: '.products-top-btns__prev-1',
   },
 
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    481: {
+      slidesPerView: 2,
+    },
+    981: {
+      spaceBetween: 18,
+      slidesPerView: 3,
+    }
+  }
+
 });
 const swiper2 = new Swiper('.products-slider-2', {
-  loop: true,
+  loop: false,
   spaceBetween: 18,
   slidesPerView: 3,
 
@@ -18,5 +32,19 @@ const swiper2 = new Swiper('.products-slider-2', {
     nextEl: '.products-top-btns__next-2',
     prevEl: '.products-top-btns__prev-2',
   },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    481: {
+      slidesPerView: 2,
+    },
+    981: {
+      spaceBetween: 18,
+      slidesPerView: 3,
+    }
+  }
 
 });
